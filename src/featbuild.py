ea298b8cf92acd7856678c31ecb7541f59549f15
gabriele16 <gabrielet85@gmail.com>
2022-06-04 19:36:11 +0200

    started buildfeatures

---
+++ src/featbuild.py
@@ -0,0 +1,12 @@
+import json
+import sys
+import pandas as pd
+import numpy as np
+import math
+from datetime import datetime,timedelta
+import time
+import matplotlib.pyplot as plt
+import mplfinance as mpf
+from sklearn.preprocessing import MinMaxScaler
+from tensorflow import keras
+import tensorflow as tf
