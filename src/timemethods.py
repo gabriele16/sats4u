6b81807cfd7a45a0f6c93da96993ffd865a490d9
gabriele16 <gabrielet85@gmail.com>
2022-06-06 17:33:11 +0200

    methods for time series

---
+++ src/timemethods.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+
+def log_return(series, periods=1):
+    log_ret = np.log(series).diff(periods=periods)
+    if periods < 0:
+        log_ret*= -1.
+    return log_ret
