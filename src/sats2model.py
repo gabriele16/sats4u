b0c37e2e507f36e09143abab213f2c8238090295
gabriele16 <gabrielet85@gmail.com>
2022-06-11 18:23:16 +0200

    modified train_test_split to allow no split

--- src/sats2model.py
+++ src/sats2model.py
@@ -21,16 +21,22 @@ class ModelTimeSeries():
 
         split_point = int(len(self.x_candles) * self.split_fraction)
         self.split_point = split_point       
-        self.x_train_candles = np.asarray(self.x_candles[:split_point], dtype=np.float32)
-        self.x_train_time = np.asarray(self.x_time[:split_point], dtype=np.float32)
-        self.y_train = np.asarray(self.y[:split_point], dtype=np.float32)
 
         if self.split_fraction == 0. or self.split_fraction == 1. : 
+
             self.split_point = 0
+            self.x_train_candles = np.asarray(self.x_candles, dtype=np.float32)
+            self.x_train_time = np.asarray(self.x_time, dtype=np.float32)
+            self.y_train = np.asarray(self.y, dtype=np.float32)
             self.x_test_candles = self.x_train_candles.copy()
             self.x_test_time = self.x_train_time.copy()
             self.y_test = self.y_train.copy()
+
         else:
+            
+            self.x_train_candles = np.asarray(self.x_candles[:split_point], dtype=np.float32)
+            self.x_train_time = np.asarray(self.x_time[:split_point], dtype=np.float32)
+            self.y_train = np.asarray(self.y[:split_point], dtype=np.float32)
             self.x_test_candles = np.asarray(self.x_candles[split_point:], dtype=np.float32)
             self.x_test_time = np.asarray(self.x_time[split_point:], dtype=np.float32)
             self.y_test = np.asarray(self.y[split_point:], dtype=np.float32)
