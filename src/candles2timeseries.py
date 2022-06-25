baa042e5eee5875496c6e9528e35fe8ae1874f6f
gabriele16 <gabrielet85@gmail.com>
2022-06-25 18:04:44 +0200

    modify timeutils to make colab notebooks leaner

--- src/candles2timeseries.py
+++ src/candles2timeseries.py
@@ -83,56 +83,56 @@ class Candle2TimeSeries():
         print(f"Feature data with time intervals 'x_time' with size : {len(self.x_time)}")
 
 
-    def backtest(self, preds, true_vals, split_point, step_back, fee=0.025):
+    # def backtest(self, preds, true_vals, split_point, step_back, fee=0.025):
 
 
-        wallet = 0
-        total_wallet_history = []
-        single_wallet_history = []
+    #     wallet = 0
+    #     total_wallet_history = []
+    #     single_wallet_history = []
 
-        buys_cnt = 0
-        buys_cnt_win = 0
-        buys_cnt_losses = 0
-        drawback = 0
-        old_profit_negative = False
-        old_profits = 0
+    #     buys_cnt = 0
+    #     buys_cnt_win = 0
+    #     buys_cnt_losses = 0
+    #     drawback = 0
+    #     old_profit_negative = False
+    #     old_profits = 0
 
-        for i in range(split_point, len(true_vals) - step_back):
-            predicted_close = preds[i - split_point]
-            previous_close = true_vals[i]
-            real_next_close = true_vals[i+1]
+    #     for i in range(split_point, len(true_vals) - step_back):
+    #         predicted_close = preds[i - split_point]
+    #         previous_close = true_vals[i]
+    #         real_next_close = true_vals[i+1]
 
-            if (previous_close + (previous_close * fee)) < predicted_close:  # buy
-                profit = real_next_close - previous_close
-                if profit > 0:
-                    profit = profit - (profit * fee)
-                    buys_cnt_win += 1
-                    old_profit_negative = False
-                else:
-                    profit = profit + (profit * fee)
-                    buys_cnt_losses += 1
-                    if old_profit_negative:
-                        old_profits += profit
-                    else:
-                        old_profits = profit
-                    if old_profits < drawback:
-                        drawback = old_profits
-                    old_profit_negative = True
-                wallet += profit
-                total_wallet_history.append(wallet)
-                single_wallet_history.append(profit)
-                buys_cnt += 1
-            else:
-                old_profit_negative = False
-                old_profits = 0
+    #         if (previous_close + (previous_close * fee)) < predicted_close:  # buy
+    #             profit = real_next_close - previous_close
+    #             if profit > 0:
+    #                 profit = profit - (profit * fee)
+    #                 buys_cnt_win += 1
+    #                 old_profit_negative = False
+    #             else:
+    #                 profit = profit + (profit * fee)
+    #                 buys_cnt_losses += 1
+    #                 if old_profit_negative:
+    #                     old_profits += profit
+    #                 else:
+    #                     old_profits = profit
+    #                 if old_profits < drawback:
+    #                     drawback = old_profits
+    #                 old_profit_negative = True
+    #             wallet += profit
+    #             total_wallet_history.append(wallet)
+    #             single_wallet_history.append(profit)
+    #             buys_cnt += 1
+    #         else:
+    #             old_profit_negative = False
+    #             old_profits = 0
 
-        print('Fee:', fee)
-        print('----------------------')
-        print('Buy     ', buys_cnt, '(', buys_cnt_win, 'ok', buys_cnt_losses, 'ko )')
-        print('No-op   ', (len(self.x_candles) - split_point) - buys_cnt)
-        print('Wallet  ', wallet)
-        print('Drawback', drawback)
+    #     print('Fee:', fee)
+    #     print('----------------------')
+    #     print('Buy     ', buys_cnt, '(', buys_cnt_win, 'ok', buys_cnt_losses, 'ko )')
+    #     print('No-op   ', (len(self.x_candles) - split_point) - buys_cnt)
+    #     print('Wallet  ', wallet)
+    #     print('Drawback', drawback)
 
-        return total_wallet_history, single_wallet_history, wallet
+    #     return total_wallet_history, single_wallet_history, wallet
 
 
