db0974a49562e6dc55ca659e049a97f8d11ca1d5
gabriele16 <gabrielet85@gmail.com>
2023-08-05 23:30:13 +0200

    updated order functions

--- sats4u/sats2trade.py
+++ sats4u/sats2trade.py
@@ -36,45 +36,62 @@ class Sats2Trade(lc.CryptoData, fb.Candles):
         self.data_folder = os.path.join(root_dir, "data")
         self.asset_details = pd.read_csv(os.path.join(root_dir, "data", "asset_details.csv"))
 
-    # Function to place a buy order
-    def place_buy_order(self, quantity):
+    # Function to place an order
+    def open_position(self, quantity, order_side="BUY",is_isolated_margin="FALSE"):
         try:
             order = self.binance_client.create_order(
                 symbol=self.crypto_pair,
-                side='BUY',
-                type='MARKET',
-                quantity=quantity
-            )
-            logging.info(f"Buy order {order['orderId']} placed successfully: {order} at {tu.get_utc_timestamp()}")
-        except Exception as e:
-            logging.info("Error placing buy order:", e)
-
-    # Function to place a sell order
-    def place_sell_order(self, quantity):
-        try:
-            order = self.binance_client.create_order(
-                symbol=self.crypto_pair,
-                side='SELL',
-                type='MARKET',
-                quantity=quantity
-            )
-            logging.info(f"Sell order {order['orderId']} placed successfully: {order} at {tu.get_utc_timestamp()}")
-        except Exception as e:
-            logging.info("Error placing sell order:", e)
-
-    # Function to place a short sell order
-    def place_short_sell_order(self, quantity):
-        try:
-            order = self.binance_client.create_order(
-                symbol=self.crypto_pair,
-                side='SELL',
+                side=order_side,
                 type='MARKET',
                 quantity=quantity,
-                isIsolated='TRUE'  # Enable this if using isolated margin for short selling
+                isIsolatedMargin=is_isolated_margin
             )
-            logging.info(f"Short sell order {order['orderId']} placed successfully: {order} at {tu.get_utc_timestamp()}")
+            logging.info(f"{order_side} order {order['orderId']} placed successfully: {order} at {tu.get_utc_timestamp()}")
         except Exception as e:
-            logging.info("Error placing short sell order:", e)
+            logging.info(f"Error placing {order_side} order:", e)
+
+    def close_position(self, quantity, order_side = "SELL"):
+        try:
+            order = self.binance_client.create_order(
+                symbol=self.crypto_pair,
+                side=order_side,  # To close a short position, you buy the same amount of the asset
+                type='MARKET',
+                quantity=quantity
+            )
+            if order_side == "SELL":
+                position_type = "LONG"
+            elif order_side == "BUY":
+                position_type = "SHORT"
+            logging.info(f"{position_type} position closed successfully: {order} at {tu.get_utc_timestamp()}")
+        except Exception as e:
+            logging.error(f"Error closing {position_type} position:", e)
+
+    # # Function to place a sell order
+    # def place_sell_order(self, quantity):
+    #     try:
+    #         order = self.binance_client.create_order(
+    #             symbol=self.crypto_pair,
+    #             side='SELL',
+    #             type='MARKET',
+    #             quantity=quantity
+    #         )
+    #         logging.info(f"Sell order {order['orderId']} placed successfully: {order} at {tu.get_utc_timestamp()}")
+    #     except Exception as e:
+    #         logging.info("Error placing sell order:", e)
+
+    # # Function to place a short sell order
+    # def place_short_sell_order(self, quantity):
+    #     try:
+    #         order = self.binance_client.create_order(
+    #             symbol=self.crypto_pair,
+    #             side='SELL',
+    #             type='MARKET',
+    #             quantity=quantity,
+    #             isIsolatedMargin='TRUE'  # Enable this if using isolated margin for short selling
+    #         )
+    #         logging.info(f"Short sell order {order['orderId']} placed successfully: {order} at {tu.get_utc_timestamp()}")
+    #     except Exception as e:
+    #         logging.info("Error placing short sell order:", e)
 
     # Function to update the technical indicators and signals
     def update_trade_signal(self):
@@ -84,6 +101,10 @@ class Sats2Trade(lc.CryptoData, fb.Candles):
         in_step = self.rollwindow
         last_step = self.candles.shape[0]
         signals_df = self.get_vma_dataframe(in_step, last_step)
+        if not os.path.isfile("signals.csv"):
+            signals_df.to_csv('signals.csv', mode='a', header=True, index=False)
+        else:
+            signals_df.iloc[[-1]].to_csv('signals.csv', mode='a', header=False, index=False)
         return signals_df        
 
     def get_portfolio(self, balances, model_signal = 0, 
@@ -108,16 +129,19 @@ class Sats2Trade(lc.CryptoData, fb.Candles):
             self.initial_balance = total_value
 
         current_returns = (total_value / self.initial_balance) - 1
-        cumulative_returns = current_returns + 1
+        if not hasattr(self, 'cumulative_returns'):
+            cumulative_returns = 0.0
+        else:
+            cumulative_returns = (1 + self.cumulative_returns) * (1 + current_returns) - 1
+        self.cumulative_returns = cumulative_returns
 
         # If portfolio_df is not defined, create it
         if not hasattr(self, 'portfolio_df'):
             self.portfolio_df = pd.DataFrame(columns=["Date", "Total Balance",
                                                        "Returns", "Cumulative Returns",
                                                        "Model Returns", "Cumulative Model Returns",
-                                                       "Model Signal"])
-            self.portfolio_df.to_csv('portfolio.csv', mode='a', header=True, index=False)
-                                           
+                                                       "Model Signal"])           
+            self.portfolio_df.to_csv('portfolio.csv', header=True, index=False)
 
         # Append the new data to the portfolio_df
         self.portfolio_df = self.portfolio_df.append(
@@ -132,15 +156,17 @@ class Sats2Trade(lc.CryptoData, fb.Candles):
             },
             ignore_index=True,
         )
-        self.portfolio_df.to_csv('portfolio.csv', mode='a', header=False, index=False)
 
+        self.portfolio_df.iloc[[-1]].to_csv('portfolio.csv', mode='a', header=False, index=False)
 
         return total_value, current_returns, cumulative_returns
 
     def trade_loop(self, quantity):
+        iteration = 0
         previous_signal = 0
         current_position = 0
         balances = self.get_account_balance(self.assets_to_trade)
+        balances.to_csv(f'account_balance_{iteration}.csv', header=True, index=False)
         self.initial_balance, current_returns, cumulative_returns = self.get_portfolio(balances)  # Store the initial balance for calculating returns
         # Set up logging
         logging.basicConfig(filename='trade_loop.log', level=logging.INFO,
@@ -161,43 +187,51 @@ class Sats2Trade(lc.CryptoData, fb.Candles):
                 # Get the last signal (latest entry in the DataFrame)
                 last_signal = signals_df.iloc[-1]['Signal']
                 # logging.info("Signal DataFrame:")
-                # logging.info(signals_df.tail(1))
+                logging.info(f"Last signal: {last_signal}")
                 # Append signals_df to signals.csv
-                signals_df.to_csv('signals.csv', mode='a', header=True, index=False)
+                signals_df.to_csv('signals.csv', header=True, index=False)
                 model_current_returns = signals_df.iloc[-1]['Returns']
                 model_cumulative_returns = signals_df.iloc[-1]['Cumulative Returns']
 
                 # If the signal changes from 0 to 1, place a buy order
                 if previous_signal == 0 and last_signal == 1:
-                    self.place_buy_order(quantity)
+                    self.open_position(quantity, side='BUY', is_isolated_margin = "FALSE")
                     # Calculate the value of the order in USDT and update the balance
                     order_value = quantity * self.get_last_price(self.crypto_pair)
                     self.initial_balance -= order_value
                     current_position = 1
+                    logging.info(f"Placed a buy order for {quantity} {self.crypto_pair} at {tu.get_utc_timestamp()}")
+                    logging.info(f"Order value in {self.reference_currency}: {order_value}")
 
                 # If the signal changes from 1 to 0 and there is an open position, close the position
                 if previous_signal == 1 and last_signal == 0 and current_position == 1:
-                    self.place_sell_order(quantity)
+                    self.close_position(quantity, side='SELL')
                     # Calculate the value of the order in USDT and update the balance
                     order_value = quantity * self.get_last_price(self.crypto_pair)
                     self.initial_balance += order_value
                     current_position = 0
+                    logging.info(f"Closed position for {quantity} {self.crypto_pair} at {tu.get_utc_timestamp()}")
+                    logging.info(f"Order value in {self.reference_currency}: {order_value}")                    
 
                 # If the signal changes from 0 to -1, place a short sell order
                 if previous_signal == 0 and last_signal == -1:
-                    self.place_short_sell_order(quantity)
+                    self.open_position(quantity, order_side='SELL', is_isolated_margin = "TRUE")
                     # Calculate the value of the order in USDT and update the balance
                     order_value = quantity * self.get_last_price(self.crypto_pair)
                     self.initial_balance -= order_value
                     current_position = -1
+                    logging.info(f"Placed a short sell order for {quantity} {self.crypto_pair} at {tu.get_utc_timestamp()}")
+                    logging.info(f"Order value in {self.reference_currency}: {order_value}")
 
                 # If the signal changes from -1 to 0 and there is an open short position, close the short position
                 if previous_signal == -1 and last_signal == 0 and current_position == -1:
-                    self.place_sell_order(quantity)
+                    self.close_position(quantity, order_side='BUY')
                     # Calculate the value of the order in USDT and update the balance
                     order_value = quantity * self.get_last_price(self.crypto_pair)
                     self.initial_balance += order_value
                     current_position = 0
+                    logging.info(f"Closed short position for {quantity} {self.crypto_pair} at {tu.get_utc_timestamp()}")
+                    logging.info(f"Order value in {self.reference_currency}: {order_value}")
 
                 # Calculate the portfolio details
                 balances = self.get_account_balance(self.assets_to_trade)
@@ -206,7 +240,11 @@ class Sats2Trade(lc.CryptoData, fb.Candles):
                                                                                          model_current_returns=model_current_returns,
                                                                                          model_cumulative_returns=model_cumulative_returns)
                 logging.info("Portfolio DataFrame:")
-                logging.info(self.portfolio_df.tail(1))
+                logging.info(f"Total Balance: {total_balance}")
+                logging.info(f"Current Returns: {current_returns}")
+                logging.info(f"Cumulative Returns: {cumulative_returns}")
+                iteration += 1
+                balances.to_csv(f'account_balance_{iteration}.csv', header=True, index=False)
 
                 # Set the previous signal for the next iteration
                 previous_signal = last_signal
@@ -214,6 +252,6 @@ class Sats2Trade(lc.CryptoData, fb.Candles):
                 current_position = last_signal
 
                 # Sleep for a certain period before the next iteration
-                time.sleep(60)  # Sleep for 60 seconds (adjust this based on your strategy)
+                #time.sleep(60)  # Sleep for 60 seconds (adjust this based on your strategy)
             #except Exception as e:
             #    print("Error occurred:", e)
